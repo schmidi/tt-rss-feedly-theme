4e347ee4d1bb45ceecfcf702db26e6deb75bafeb
Tobias Schmid <schmid.tobias@bluewin.ch>
2019-01-14 22:06:52 +0100

    update gitignore, update install script

--- install.sh
+++ install.sh
@@ -4,4 +4,8 @@ cd ..
 # TODO: check if pwd is themes.local
 ln -s tt-rss-feedly-theme/feedly.css
 ln -s tt-rss-feedly-theme/feedly-night.css
+ln -s tt-rss-feedly-theme/feedly-ocean.css
+ln -s tt-rss-feedly-theme/feedly-paper.css
+ln -s tt-rss-feedly-theme/feedly-solarized-dark.css
+ln -s tt-rss-feedly-theme/feedly-solarized-light.css
 ln -s tt-rss-feedly-theme/feedly
